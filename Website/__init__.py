5c4cf3a2a8fe1bf960d4ebc17529a51e4b1a14a5
mothraelite <milaninavid@gmail.com>
2021-05-08 09:22:39 -0700

    Added:\n\nChanged:\n\t-genereateInit now properly generates the routing file including home rerouting.\n\nTODO:\n\t-GenereateInit needs to be able to generate the navbar but is exhibiting strange behavior so its currently ommited.

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,34 +1,41 @@
 import os
 import pymongo
-from flask import Flask
+import Website.templates.home.home
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config['MONGO_DBNAME'] = 'beat_warrior'
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/beat_warrior"
-	
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+	# create and configure the app
+	app = Flask(__name__, instance_relative_config=True);
+	app.config['MONGO_DBNAME'] = 'beat_warrior';
+	app.config["MONGO_URI"] = "mongodb://localhost:27017/beat_warrior"
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+	if test_config is None:
+		app.config.from_pyfile('config.py', silent=True);
+	else:
+		app.config.from_mapping(test_config);
+	try:
+			os.makedirs(app.instance_path);
+	except OSError:
+		pass;
 
-	#mango test connection
-    mongoDB = PyMongo(app);
-    beatCollection = mongoDB.db.test.find();
-    
-    # Home page
-    @app.route('/', methods=['GET', 'POST'])
-    def home():
-        return beatCollection[0]["title"];
+	mongoDB = PyMongo(app);
+	beatCollection = mongoDB.db.test.find();
 
-    return app;
+	@app.route("/", methods=["GET"])
+	def homeRoute():
+		return redirect("/home",302);
+	@app.route("/home", methods=["GET", "POST"])
+	def home():
+		try:
+			home.preRender();
+		except Exception as error:
+			print(error);
+		temp =  render_template("home/home.html");
+		try:
+			home.postRender();
+		except Exception as error:
+			print(error);
+		return temp;
+
+	return app
--- Website/generateInit.py
+++ Website/generateInit.py
@@ -10,48 +10,57 @@ import os;
         Go through it and define routes+extra templates/resources associated with the page
 '''
 
-#Create the base page in text
-toGenerate = """
-import os
-import pymongo
-from flask import Flask
-from flask_pymongo import PyMongo
+#Config options
+config = {};
+config["devActive"] = True;
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True);
-    app.config['MONGO_DBNAME'] = 'beat_warrior';
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/beat_warrior";
-	
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True);
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config);
+toBeWritten = {};
+toBeWritten["imports"]="import os\nimport pymongo\n";
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path);
-    except OSError:
-        pass;
+toBeWritten["initMeat"]="\t@app.route(\"/\", methods=[\"GET\"])\n";
+toBeWritten["initMeat"]+="\tdef homeRoute():\n\t\treturn redirect(\"/home\",302);\n"
 
-	#mango connection
-    mongoDB = PyMongo(app);
-    beatCollection = mongoDB.db.test.find();
+toBeWritten["afterImports"]="from flask import Flask, render_template, redirect\nfrom flask_pymongo import PyMongo\n\ndef create_app(test_config=None):\n";
+toBeWritten["afterImports"]+="\t# create and configure the app\n\tapp = Flask(__name__, instance_relative_config=True);\n";
+toBeWritten["afterImports"]+="\tapp.config['MONGO_DBNAME'] = 'beat_warrior';\n\tapp.config[\"MONGO_URI\"] = \"mongodb://localhost:27017/beat_warrior\"\n\n";
+toBeWritten["afterImports"]+="\tif test_config is None:\n\t\tapp.config.from_pyfile('config.py', silent=True);\n";
+toBeWritten["afterImports"]+="\telse:\n\t\tapp.config.from_mapping(test_config);\n";
+toBeWritten["afterImports"]+="\ttry:\n\t\t\tos.makedirs(app.instance_path);\n\texcept OSError:\n\t\tpass;\n\n";
+toBeWritten["afterImports"]+="\tmongoDB = PyMongo(app);\n\tbeatCollection = mongoDB.db.test.find();\n\n";
 
-        
-"""
+#create our navbar
+toBeWritten["navbar"]="""
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <a class="navbar-brand" href="/home">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#primaryNavbar">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="primaryNavbar">
+""";
 
 #read current file structure for pages
-def findPages(_dir):
+def findPages(_dir, _bufferDictionary):
+    #store pages to construct so I only have to iterate once
     for filename in os.listdir(_dir):
         #Do I have sub directories that arent components?
-        if os.path.isfile(filename):
+        if os.path.isdir(_dir+'/'+filename):
+            #gen navbar for this page
+            temp = "";
+            for i in range(_dir.count('/')):
+                temp += "\t";
+                
             #Am I component folders for the page?
-            if(filename.lower() != "css" && filename.lower() != "js"):
-                #I am not, I am sub page hi
-                findPages(_dir/filename);
+            if(filename.lower() != "css" and filename.lower() != "js"):
+                 #I am not, I am sub page hi
+                _bufferDictionary["navbar"] += temp+"<li class=\"nav-item\">\n";
+                _bufferDictionary["navbar"] += temp+"\t<a class=\"nav-link\" href=\""+filename+"\">"+filename+"</a>\n";
+
+                _bufferDictionary["navbar"] += temp+"<ul class=\"navbar-nav\">\n";
+                findPages(_dir+'/'+filename, _bufferDictionary);
+                _bufferDictionary["navbar"] += temp+"</ul>\n";
+                
+            #close navbar for this page
+            _bufferDictionary["navbar"] += temp+"</li>\n";
         else:
             #Find config file to start processing
             if(filename.lower() == "config.txt"):
@@ -62,25 +71,76 @@ def findPages(_dir):
                 #2) String | argument url header //has formatting done already
                 #2) String | Arguments list
                 #3) String | url methods
-                file = open(filename, 'r');
+                file = open(_dir+'/'+filename, 'r');
                 
-                name = file.readline();
-                argumentHeader = file.readline();
-                args = file.readline();
-                urlmethods = file.readline();
-                
+                spl = file.readlines();
+
                 file.close();
                 
-                constructPage(name, _dir, argumentHeader, args, urlmethods);
-            
-    return toGenerate;
+                constructPage(spl[0].replace('\n',""), _dir, spl[1].replace('\n',""), spl[2].replace('\n',""), _bufferDictionary, spl[3].replace('\n',""));
                 
 #helper function: construct a page
 #------------------------------------------
 #return: string | to append to tobe generated file
-def constructPage(_name, _dir, _argumentHeaders, _args, _urlmethods=["GET"]):
-        toGenerate += "\t@app.route(\"/"+_dir+'/'+_name+_argumentHeaders+"\", methods="+_urlmethods+")\n";
-        toGenerate += "\tdef " + _name +  args +":\n";
-        toGenerate += "\t\t";
+def constructPage(_name, _dir, _argumentHeaders, _vals, _bufferDict, _urlmethods=["GET"]):
+    #import custom code if present to be called when routed
+    foundPythonCode = False;
+    if os.path.exists(_dir+'/'+_name+".py"):
+        foundPythonCode = True;
+        tdir = _dir[_dir.find("Website"):len(_dir)].replace("/",".");
+        _bufferDict["imports"] += "import " + tdir+'.'+_name+"\n";
+
+    _dir = _dir[54:len(_dir)];
     
+    if _argumentHeaders != "":
+        _argumentHeaders = "/"+_argumentHeaders;
+        
+    _bufferDict["initMeat"] += "\t@app.route(\"/"+_dir+_argumentHeaders+"\", methods="+_urlmethods+")\n";
+    _bufferDict["initMeat"] += "\tdef " + _name +  _vals +"("+_argumentHeaders.replace("{","").replace("}","").replace("/","")+"):\n";
+        
+    if foundPythonCode:
+        # preserved code
+        _bufferDict["initMeat"] += "\t\ttry:\n";
+        _bufferDict["initMeat"] += "\t\t\t"+_name+".preRender();\n";
+        _bufferDict["initMeat"] += "\t\texcept Exception as error:\n";
+        if config["devActive"]:
+            toBeWritten["initMeat"] += "\t\t\tprint(error);\n";
+        #_bufferDict["initMeat"] += "\t\telse:\n";
+        
+        _bufferDict["initMeat"] += "\t\ttemp =  render_template(\""+_dir+'/'+_name+".html\");\n";
+        
+        #postproccessed code
+        _bufferDict["initMeat"] += "\t\ttry:\n";
+        _bufferDict["initMeat"] += "\t\t\t"+_name+".postRender();\n";
+        _bufferDict["initMeat"] += "\t\texcept Exception as error:\n";
+        if config["devActive"]:
+            _bufferDict["initMeat"] += "\t\t\tprint(error);\n";
+        #_bufferDict["initMeat"] += "\t\telse:\n";
+        
+        _bufferDict["initMeat"] += "\t\treturn temp;\n";
+    else:
+        _bufferDict["initMeat"] += "\t\treturn render_template(\""+_dir+'/'+_name+".html\");\n";
+
+
+#find all pages and process them
+findPages("C:/Users/hmila/Desktop/Beat Warrior/Website/templates", toBeWritten);
+
+#combine everything
+    #routing file
+toBeWritten["routing"] = toBeWritten["imports"] + toBeWritten["afterImports"] + toBeWritten["initMeat"] + "\n\treturn app";
     
+    #navbar
+toBeWritten['navbar'] += """
+        </ul>
+    </div>
+</nav>
+"""
+
+#create actual files
+file = open("testNavbar.html", "w");
+file.write(toBeWritten["navbar"]);
+file.close();
+
+file = open("__init__.py", "w");
+file.write(toBeWritten["routing"]);
+file.close();
